5373f152fd0ced5ddb12775cd2787a593bf0eacd
MUHAMMAD ZIKRI BIN MOHD YUNAN <45089636+DrPolymath@users.noreply.github.com>
2020-05-01 14:49:41 +0800

    Login and Sign Up Pages Development

--- Organiser.js
+++ Organiser.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
     e.preventDefault();
   });
 });
+
+//Organiser - Add Game
+$('#customFile').on('change',function(){
+  //get the file name
+  var fileName = $(this).val();
+  //replace the "Choose a file" label
+  $(this).next('.custom-file-label').html(fileName);
+})
