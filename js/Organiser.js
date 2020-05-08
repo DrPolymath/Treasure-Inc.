b655be569c1df47097e84b012bcb4e8d420133bb
MUHAMMAD ZIKRI BIN MOHD YUNAN <45089636+DrPolymath@users.noreply.github.com>
2020-05-08 18:07:17 +0800

    Change image upload interface

--- js/Organiser.js
+++ js/Organiser.js
@@ -46,3 +46,33 @@ function deleteAccount(){
 $(".showGamesCard").click(function(){
   $("#gameDetailModal").modal("show")
 });
+
+function triggerClick(){
+  document.querySelector("#gamePicture").click();
+}
+
+function displayImage(e) {
+  if (e.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e){
+      document.querySelector('#gamePicUpload').setAttribute('src', e.target.result);
+    }
+    reader.readAsDataURL(e.files[0]);
+  }
+}
+
+function triggerClick(){
+  $("#updateGameModal").modal("hide")
+  document.querySelector("#gamePictureUpdate").click();
+}
+
+function displayImage(e) {
+  if (e.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e){
+      document.querySelector('#gamePicUploadUpdate').setAttribute('src', e.target.result);
+    }
+    reader.readAsDataURL(e.files[0]);
+  }
+  $("#updateGameModal").modal("show")
+}
